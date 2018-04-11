1aadaa68056655e06841769385d210b02ae6c549
TerrySu <2684759855@qq.com>
2018-04-11 14:11:40 +0800

    Fix: [Utils-RemoveExtraPointsOnSameSegmentLine] Logic

--- src/util/geometry/removeExtraPointsOnSameSegmentLine.ts
+++ src/util/geometry/removeExtraPointsOnSameSegmentLine.ts
@@ -1,13 +1,14 @@
-import { isLast, isFirst } from '../array';
-import MathVector from '../math/MathVector';
-import isPointOnSegmemtLine from './isPointOnSegmemtLine';
-import { cloneDeep } from 'lodash';
-import isPointEqual from './isPointEqual';
+import { isLast, isFirst } from "../array"
+import MathVector from "../math/MathVector"
+import isPointOnSegmemtLine from "./isPointOnSegmemtLine"
+import { cloneDeep } from "lodash"
+import isPointEqual from "./isPointEqual"
 /**
  * Remove points that are on a same segment line which it's next point is on.
  */
-export default function ( points: Point2D[] ) {
+export default function( points: Point2D[] ) {
 	let res: Point2D[] = cloneDeep( points )
+	let prevNotRepeated: Point2D
 
 	const { length }: Point2D[] = points
 
@@ -21,17 +22,24 @@ export default function ( points: Point2D[] ) {
 		let res: boolean = true
 		const { length }: Point2D[] = points
 
-		if ( ! isFirst( index ) && ! isLast( index, length ) ) {
+		if ( isFirst( index ) ) {
+			prevNotRepeated = point
+		}
+
+		if ( !isFirst( index ) && !isLast( index, length ) ) {
 			const prev: Point2D = points[ index - 1 ]
 			const next: Point2D = points[ index + 1 ]
 
 			if ( isPointEqual( point, next ) ) {
 				res = false
 			}
-			if ( ! isPointEqual( point, next )  ) {
-				if ( ! isPointEqual( point, prev ) && isPointOnSegmemtLine( point, [ prev, next ] ) ) {
+
+			if ( !isPointEqual( point, next ) ) {
+				if ( isPointOnSegmemtLine( point, [ prevNotRepeated, next ] ) ) {
 					res = false
 				}
+
+				prevNotRepeated = point
 			}
 		}
 
