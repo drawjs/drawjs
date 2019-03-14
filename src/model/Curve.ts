78b34d84e20325be8609e4c218fbe3035e0c6a86
TerrySu <2684759855@qq.com>
2019-03-14 16:11:33 +0800

    adjust Curve's render method

--- src/model/Curve.ts
+++ src/model/Curve.ts
@@ -42,24 +42,37 @@ export default class Curve extends Cell {
 		const { points = [] } = props
 
 		if ( points.length > 0 ) {
-			this.segment1  = this.sharedGetters.createSegmentByPoint( points[ 0 ], this.draw, { showHandle: false } )
-			this.segment2  = this.sharedGetters.createSegmentByPoint( points[ 1 ], this.draw, { showHandle: false } )
+			this.segment1 = this.sharedGetters.createSegmentByPoint(
+				points[ 0 ],
+				this.draw,
+				{ showHandle: false }
+			)
+			this.segment2 = this.sharedGetters.createSegmentByPoint(
+				points[ 1 ],
+				this.draw,
+				{ showHandle: false }
+			)
 		} else {
 			this.segment1 = props.segment1
 			this.segment2 = props.segment2
 		}
 
-		
 		this.handle1 = this.segment1.handleOut
 		this.handle2 = this.segment2.handleIn
 
 		this.path = props.path
 
-		if ( notNil( props.setLineDash ) )  { this.setLineDash = props.setLineDash }
+		if ( notNil( props.setLineDash ) ) {
+			this.setLineDash = props.setLineDash
+		}
 
 		const { curveUsesCanvasApi, curveRate } = this.drawStore.setting
-		this.useCanvasCurve = notNil( curveUsesCanvasApi ) ? curveUsesCanvasApi : false
-		this.useCanvasCurve = notNil( props.useCanvasCurve ) ? props.useCanvasCurve : this.useCanvasCurve
+		this.useCanvasCurve = notNil( curveUsesCanvasApi ) ?
+			curveUsesCanvasApi :
+			false
+		this.useCanvasCurve = notNil( props.useCanvasCurve ) ?
+			props.useCanvasCurve :
+			this.useCanvasCurve
 
 		this.t = notNil( curveRate ) && curveRate !== 0 ? curveRate : 1
 		this.t = notNil( props.t ) && props.t !== 0 ? props.t : this.t
@@ -100,13 +113,28 @@ export default class Curve extends Cell {
 	get path2d(): Path2D {
 		let path2d = new Path2D()
 
-		const { handle1Point, handle2Point, point1, point2, useCanvasCurve, t } = this
+		const {
+			handle1Point,
+			handle2Point,
+			point1,
+			point2,
+			useCanvasCurve,
+			t
+		} = this
 
-		!useCanvasCurve && bezierCurve( [ point1, handle1Point, handle2Point, point2 ], t, path2d )
+		!useCanvasCurve &&
+			bezierCurve( [ point1, handle1Point, handle2Point, point2 ], t, path2d )
 
 		if ( useCanvasCurve ) {
 			path2d.moveTo( point1.x, point1.y )
-			path2d.bezierCurveTo( handle1Point.x, handle1Point.y, handle2Point.x, handle2Point.y, point2.x, point2.y )
+			path2d.bezierCurveTo(
+				handle1Point.x,
+				handle1Point.y,
+				handle2Point.x,
+				handle2Point.y,
+				point2.x,
+				point2.y
+			)
 		}
 
 		return path2d
@@ -137,12 +165,17 @@ export default class Curve extends Cell {
 
 	render() {
 		const { ctx } = this.getters
-		const { setLineDash, lineWidth } = this
-		ctx.save()
-		ctx.lineWidth = lineWidth
-		ctx.strokeStyle = "#00f0ff"
-		if ( notNil( setLineDash ) ) { ctx.setLineDash( setLineDash ) }
-		ctx.stroke( this.path2d )
-		ctx.restore()
+		const { setLineDash, lineWidth, strokeColor } = this
+
+		if ( strokeColor ) {
+			ctx.save()
+			ctx.lineWidth = lineWidth
+			ctx.strokeStyle = "#00f0ff"
+			if ( notNil( setLineDash ) ) {
+				ctx.setLineDash( setLineDash )
+			}
+			ctx.stroke( this.path2d )
+			ctx.restore()
+		}
 	}
 }
